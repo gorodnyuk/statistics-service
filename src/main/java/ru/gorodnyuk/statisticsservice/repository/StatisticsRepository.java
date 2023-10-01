08fcb4ce7446c3d79b8ed548647e47e2600720bf
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-10-01 17:58:52 +0300

    Доработка логики подсчета статистики

--- src/main/java/ru/gorodnyuk/statisticsservice/repository/StatisticsRepository.java
+++ src/main/java/ru/gorodnyuk/statisticsservice/repository/StatisticsRepository.java
@@ -1,11 +1,17 @@
 package ru.gorodnyuk.statisticsservice.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import ru.gorodnyuk.statisticsservice.model.StatisticsEntity;
 
 public interface StatisticsRepository extends CrudRepository<StatisticsEntity, Long> {
 
     boolean existsByIp(String ip);
 
-    void updateStatisticByIp(String ip);
+    @Query(value = "UPDATE statistics " +
+            "SET counter = counter + 1 " +
+            "WHERE ip = :ip",
+            nativeQuery = true)
+    void updateStatisticByIp(@Param("ip") String ip);
 }
