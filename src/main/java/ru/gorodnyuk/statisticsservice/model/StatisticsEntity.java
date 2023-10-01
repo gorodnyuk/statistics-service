063f8b227d1b42f5d9a485b8ad60c89d2dc60732
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-10-01 18:41:16 +0300

    Рефакторинг. Убраны лишние аннотации

--- src/main/java/ru/gorodnyuk/statisticsservice/model/StatisticsEntity.java
+++ src/main/java/ru/gorodnyuk/statisticsservice/model/StatisticsEntity.java
@@ -6,9 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.NoArgsConstructor;
 
-@NoArgsConstructor
 @Entity
 @Table(name = "statistics")
 public class StatisticsEntity {
