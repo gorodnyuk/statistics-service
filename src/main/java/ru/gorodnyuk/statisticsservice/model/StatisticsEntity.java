b35dc757ea0c53bfaf1d3d8d2d0d7b7a2842c36e
gorodnyuk <sergeygorodnyuk@yandex.ru>
2023-10-01 18:25:32 +0300

    Доработка логики подсчета статистики

--- src/main/java/ru/gorodnyuk/statisticsservice/model/StatisticsEntity.java
+++ src/main/java/ru/gorodnyuk/statisticsservice/model/StatisticsEntity.java
@@ -3,6 +3,7 @@ package ru.gorodnyuk.statisticsservice.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.NoArgsConstructor;
@@ -13,14 +14,14 @@ import lombok.NoArgsConstructor;
 public class StatisticsEntity {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "ip")
     private String ip;
 
     @Column(name = "counter")
-    private Long counter;
+    private Long counter = 1L;
 
     public StatisticsEntity(String ip) {
         this.ip = ip;
